4335ccff991dd8ccdfae63101ef6ed08f771d8cc
Manoj Vyavahare <manojvyavahare27@gmail.com>
2025-10-02 21:04:52 -0700

    Push bdate

--- Pages/PatientDomain/PatientSearch.js
+++ Pages/PatientDomain/PatientSearch.js
@@ -331,8 +331,8 @@ class PatientSearch
     async selectBornDate(pat_dob)
     {   //await this.txtbox_BornDate.fill('')
         //await this.txtbox_BornDate.fill(pat_dob)
-        //await this.page.getByPlaceholder('dd/mm/yyyy').fill("22/09/2024")
-        await this.page.locator("xpath=//input[@id='Date of Birth']").fill("22/09/2023");
+        await this.page.getByPlaceholder('dd/mm/yyyy').fill("22/09/2024")
+       // await this.page.locator("xpath=//input[@id='Date of Birth']").fill("22/09/2023");
         await this.page.keyboard.press("Tab");
 
     }
